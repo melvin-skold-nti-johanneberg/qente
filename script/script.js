3fdd0de17cba1512c150dcdafd99d9c2a1e1ede2
olle-henriksson-wiren-nti-johanneberg <olle.henrikssonwiren@elev.ga.ntig.se>
2022-10-19 17:37:59 +0200

    cool animation

--- script/script.js
+++ script/script.js
@@ -6,3 +6,18 @@ function scrollArrow(x, y) {
     window.scrollTo(x, y);
 }
 
+window.onscroll = function() {navScroll()};
+function navScroll() {
+    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+        
+        document.querySelector(".nav").style.opacity = "0%";
+        document.querySelector(".nav").style.top = "-100px";
+
+    } else {
+        
+        document.querySelector(".nav").style.opacity = "100%";
+        document.querySelector(".nav").style.top = "0px";
+
+    }
+}
+navScroll()
